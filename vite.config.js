0a265e30eab423db4d86164748274e4d4d04cb84
Coder Haider <haiderali98765012@gmail.com>
2024-08-04 16:59:26 +0500

    deleted files commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
   base: '/display-cards/', // replace 'my-app' with your repository name  
+  plugins: [react()],
 });
